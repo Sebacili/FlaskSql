62cb40cdd6d8632bfcb4b1deadce2434b3237205
Cilibeanu Sebastian <cilibeanu.nicolae@iisgalvanimi.edu.it>
2022-11-08 07:31:12 +0000

    correzione verifica 3

--- verifica3.py
+++ verifica3.py
@@ -1,26 +1,25 @@
-
-#
-from flask import Flask, render_template, request, redirect, url_for, Response, redirect
+from flask import Flask, render_template, request, Response
 app = Flask(__name__)
 
 import io
+import pandas as pd 
+import matplotlib.pyplot as plt
 from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
 from matplotlib.figure import Figure
 import matplotlib
 matplotlib.use('Agg')
-import matplotlib.pyplot as plt
-import pandas as pd
 import pymssql
 
+conn = pymssql.connect(server='213.140.22.237\SQLEXPRESS', user='cilibeanu.nicolae', password='xxx123##', database='cilibeanu.nicolae')
 
-conn = pymssql.connect(server="213.140.22.237\SQLEXPRESS", user="cilibeanu.nicolae",password="xxx123##",database="cilibeanu.nicolae")
-@app.route("/", methods=["GET"])
-def homeroute():
-    return("scrivere /bestCustomers per info")
-    
+
+@app.route("/",methods=["GET"])
+def homepage():
+    return ('digitare /bestCustomers')
+
 @app.route("/bestCustomers", methods=["GET"])
 def bestCustomers():
-    query = 'SELECT TOP 10 customers.customer_id, customers.first_name, customers.last_name ,SUM(list_price) as spesa_totale FROM sales.customers INNER JOIN sales.orders ON sales.customers.customer_id = sales.orders.customer_id INNER JOIN sales.order_items ON sales.order_items.order_id = sales.orders.order_id GROUP BY customers.customer_id, customers.first_name, customers.last_name ORDER BY SUM(list_price) DESC '
+    query = 'SELECT TOP 10 customers.customer_id, customers.first_name, customers.last_name ,SUM(list_price * quantity) as spesa_totale FROM sales.customers INNER JOIN sales.orders ON sales.customers.customer_id = sales.orders.customer_id INNER JOIN sales.order_items ON sales.order_items.order_id = sales.orders.order_id GROUP BY customers.customer_id, customers.first_name, customers.last_name ORDER BY SUM(list_price * quantity) DESC '
     #INNER JOIN sales.oder_items ON sales.orders.order_id = sales.order_items.order_id
     tabella = pd.read_sql(query,conn)
     print(tabella.columns.values)
